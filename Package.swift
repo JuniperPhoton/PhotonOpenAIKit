b7234a2985823fdd6cf1b9054a6b7802db72f9cb
juniperphoton <dengweichao@hotmail.com>
2023-03-10 22:44:54 +0800

    Support iOS 14 and above only

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PhotonOpenAIKit",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
         .macOS(.v11),
     ],
     products: [
