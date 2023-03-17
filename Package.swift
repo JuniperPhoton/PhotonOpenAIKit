349146661560c7456a18457687e3ed6fe1d5fbfe
juniperphoton <dengweichao@hotmail.com>
2023-03-17 17:46:15 +0800

    Use the exact version of AlamofireEventSource

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: .init(5, 0, 0))),
-        .package(url: "https://github.com/JuniperPhoton/AlamofireEventSource", branch: "main"),
+        .package(url: "https://github.com/JuniperPhoton/AlamofireEventSource", exact: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
