e5b12b1810f9c741e77b36c41cfd12279d12d359
juniperphoton <dengweichao@hotmail.com>
2023-03-29 18:07:50 +0800

    Add gpt-4 model

--- Sources/PhotonOpenAIBase/Base/AIModel.swift
+++ Sources/PhotonOpenAIBase/Base/AIModel.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
+/// See all models: https://platform.openai.com/docs/models/overview
 public enum AIModel: String, Codable {
     case gpt_3_5_turbo = "gpt-3.5-turbo"
     case gpt_3_5_turbo_0301 = "gpt-3.5-turbo-0301"
     case text_davinci_003 = "text-davinci-003"
     case text_davinci_002 = "text-davinci-002"
     case code_davinci_002 = "code-davinci-002"
+    case gpt_4 = "gpt-4"
 }
